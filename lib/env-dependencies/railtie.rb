deb31378aa54667aa5ae5cd2ee130a9e954adf8c
Luke Horvat <lukehorvat@gmail.com>
2016-03-18 16:19:10 +1100

    Disable checks when user hasn't defined any dependencies

--- lib/env-dependencies/railtie.rb
+++ lib/env-dependencies/railtie.rb
@@ -1,6 +1,8 @@
 module EnvDependencies
   class Railtie < Rails::Railtie
     config.before_initialize do
+      next unless config.respond_to?(:env_dependencies)
+
       config.env_dependencies.select do |dependency|
         !ENV.key?(dependency)
       end.each do |dependency|
